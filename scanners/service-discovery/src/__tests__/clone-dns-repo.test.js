0791397a76567dace13ac9faf4178f51abf64e63
Lila Kelland <lila.kelland@gmail.com>
2023-09-08 16:30:09 -0400

    added basic tests for clone and remove repo

---
+++ scanners/service-discovery/src/__tests__/clone-dns-repo.test.js
@@ -0,0 +1,29 @@
+import { cloneDnsRepository, removeClonedDnsRepository } from '../clone-dns-repo.js';
+import fsExtra from 'fs-extra';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+describe('cloneDnsRepository', () => {
+    it('should clone the DNS repository successfully', async () => {
+      const repoPath = await cloneDnsRepository();  
+      
+      expect(repoPath).toBeTruthy();   
+    });
+  });
+
+
+describe('Remove cloned DNS repository', () => {
+  it('should remove the cloned DNS repository successfully', async () => {
+
+    // const repoPath = await cloneDnsRepository();
+    await removeClonedDnsRepository();
+
+    const clonedRepoPath = path.join(__dirname, 'temp-cloned-repo', 'dns');
+    const exists = await fsExtra.pathExists(clonedRepoPath);
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+   
+    expect(exists).toBe(false);
+  });
+});
