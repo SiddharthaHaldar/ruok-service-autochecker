124710ce1041d5b9f46cecffaf0f672d4363a736
Lila Kelland <lila.kelland@gmail.com>
2023-09-11 14:41:25 -0400

    added tests for clone-repo module

---
+++ scanners/github-repo-scanner/src/__tests__/clone-repo.tests.js
@@ -0,0 +1,29 @@
+// clone-repo.tests.js
+
+import { cloneRepository, removeClonedRepository } from '../clone-repo.js'; // Replace 'your-module' with the correct module path
+import fsExtra from 'fs-extra';
+
+describe('Repository cloning and removal', () => {
+  let clonedRepoPath; 
+
+  it('should clone a repository', async () => {
+    const cloneUrl = 'git@github.com:PHACDataHub/dns.git'; 
+    const repoName = 'dns';
+    clonedRepoPath = await cloneRepository(cloneUrl, repoName);
+    expect(clonedRepoPath).toBeTruthy();  
+  });
+
+
+  it('should remove the cloned repository', async () => {
+    // Ensure repoPath is defined (it should have been set in the previous test)
+    expect(clonedRepoPath).toBeDefined();
+
+    await removeClonedRepository(clonedRepoPath);
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    const directoryExists = await fsExtra.pathExists(clonedRepoPath);
+
+    expect(directoryExists).toBe(false); 
+  });
+});
+
+
