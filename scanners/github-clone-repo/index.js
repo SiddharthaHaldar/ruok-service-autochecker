a30d97205c451254ef65fba395982184c1b8803a
LilaKelland <lila.kelland@gmail.com>
2023-10-17 13:05:43 +0000

    cleaned up console logs - moved publish log to publish function

--- scanners/github-clone-repo/index.js
+++ scanners/github-clone-repo/index.js
@@ -19,33 +19,24 @@ console.log('🚀 Connected to NATS server - listening on ...', sub.subject, "ch
 
 async function publish(subject, payload) {
   nc.publish(subject, jc.encode(payload)) 
-  console.log('YAY PUBLISHED')
+  console.log(`Sent to ... ${subject}: `, payload)
 }
 
 process.on('SIGTERM', () => process.exit(0))
 process.on('SIGINT', () => process.exit(0))
 ;(async () => {
- 
     for await (const message of sub) {
-        console.log('\n**************************************************************')
-        console.log(`Recieved from ... ${message.subject} \n`)
-        
         const webhookPayload  = await jc.decode(message.data)
-        console.log(webhookPayload )
+
+        console.log('\n**************************************************************')
+        console.log(`Recieved from ... ${message.subject}: \n ${JSON.stringify(webhookPayload)}`)
 
         const { sourceCodeRepository, eventType} = webhookPayload 
         const { repoName, cloneUrl } = await extractUrlParts(sourceCodeRepository)
-     
-        // const serviceName = message.subject.split(".").reverse()[0]
         
         await cloneRepository(cloneUrl, repoName)
 
-        // console.log('NATS_PUB_STREAM: ', NATS_PUB_STREAM)
-        // console.log('service', serviceName)
-
-        // await publish(`${NATS_PUB_STREAM}.${serviceName}`, {'repoName':repoName}) 
         await publish(`${NATS_PUB_STREAM}.${repoName}`, webhookPayload) //This service clones the repo - passing on webhook info
-        console.log(`Sent to ... ${NATS_PUB_STREAM}.${repoName}: `, webhookPayload)
     }
 })();
 
