8508f8f0c319a0b57bce48810b937db113e22c0d
Collin Brown <collin.j.brown95@gmail.com>
2023-10-19 17:02:55 +0000

    fix(webhook-server): fix env var name conflict with k8s

--- webhook-server/index.js
+++ webhook-server/index.js
@@ -9,7 +9,7 @@ import 'dotenv/config'
 // Get config variables from environment
 const {
   WEBHOOK_SECRET,
-  WEBHOOK_SERVER_PORT,
+  WEBHOOK_SERVER_PORT_NUMBER,
   NATS_SERVER,
   NATS_PUB_STREAM
 } = process.env
@@ -34,6 +34,7 @@ app.post('/', async (req, res) => {
   // x-hub-signature-256 jwt that came with the request.
   if (!verifySignature(req)) {
     res.status(401).send("Unauthorized");
+    console.log("Unauthorized Request");
     return;
   }
   // extract relevant information to put into queue.
@@ -50,6 +51,6 @@ app.post('/', async (req, res) => {
   console.log("successfully published event: ", eventType)
 })
 
-app.listen(WEBHOOK_SERVER_PORT, () => {
-  console.log(`Webhook server listening on port ${WEBHOOK_SERVER_PORT}`)
+app.listen(WEBHOOK_SERVER_PORT_NUMBER, () => {
+  console.log(`Webhook server listening on port ${WEBHOOK_SERVER_PORT_NUMBER}`)
 })
