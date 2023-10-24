4d7de3cf96508ab2077c61c31af6717c9060cf39
Collin Brown <collin.j.brown95@gmail.com>
2023-10-24 22:21:16 +0000

    refactor(api): new graphql schema

--- api/index.js
+++ api/index.js
@@ -4,35 +4,35 @@ import { Server } from "./src/server.js";
 import { Database, aql } from "arangojs";
 import 'dotenv-safe/config.js'
 
-const { 
-    PORT = 4000,
-    HOST = '0.0.0.0',
-    DB_NAME = "dataServices",
-    DB_URL = "http://database:8529",
-    DB_USER = "root",
-    DB_PASS = 'yourpassword',
-  } = process.env;
+const {
+  PORT = 4000,
+  HOST = '0.0.0.0',
+  DB_NAME = "ruok",
+  DB_URL = "http://localhost:8529",
+  DB_USER = "root",
+  DB_PASS = 'yourpassword',
+} = process.env;
 
 // DB connection
 const dbConfig = {
-    url: DB_URL, 
-    databaseName: DB_NAME, 
-    auth: { username: DB_USER, password: DB_PASS },
-    createCollection: true, 
-  };
-  
+  url: DB_URL,
+  databaseName: DB_NAME,
+  auth: { username: DB_USER, password: "" },
+  createCollection: true,
+};
+
 const db = new Database(dbConfig);
 
 
 process.on('SIGTERM', () => process.exit(0))
 process.on('SIGINT', () => process.exit(0))
-;(async ( ) => {
-  const server = new Server({
-    schema,
-    context: { db},
-  })
+  ; (async () => {
+    const server = new Server({
+      schema,
+      context: { db },
+    })
 
-  server.listen({ port: PORT, host: HOST }, () =>
-    console.log(`🚀 API is running on http://${HOST}:${PORT}/graphql`),
-  )
-})()
+    server.listen({ port: PORT, host: HOST }, () =>
+      console.log(`🚀 API is running on http://${HOST}:${PORT}/graphql`),
+    )
+  })()
