1de3c8b3d8d4f8d1dd616caa0a66b06d61a8b11f
Collin Brown <collin.j.brown95@gmail.com>
2023-11-02 13:39:10 +0000

    tidy(repo): remove database folder

--- database/temp.sh
+++
@@ -1,6 +0,0 @@
-#  curl -u root:yourpassword -X POST --header 'accept: application/json' \
-#     --data '{"name": "dataServices", "createCollection": true }' \
-#     --dump - http://0.0.0.0:8529/_api/database
-
-
-curl -X POST -d "{ \"field\": \"value\" }" --dump - https://localhost:8529/_db/myDatabase/_api/document?collection=cars
