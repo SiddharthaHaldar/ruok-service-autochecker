221c01d836ef2f9854977ee2811109b107779841
Collin Brown <collin.j.brown95@gmail.com>
2023-11-02 15:56:54 +0000

    refactor(graph-updater): endpoint detection now happens in event-collectors

--- graph-updater/src/endpoint.js
+++ graph-updater/src/endpoint.js
@@ -1,7 +1,7 @@
 import { GithubEndpoint } from "./src/github-endpoint.js"
 import { WebEndpoint } from "./src/web-endpoint.js"
 import { ContainerEndpoint } from "./src/container-endpoint.js"
-import { GenericEndpoint } from "./src/generic-endpoint.js"
+import { GenericEndpoint } from "../graph-updater/src/generic-endpoint.js"
 
 export function endpoint(kind) {
   switch (kind) {
@@ -10,17 +10,4 @@ export function endpoint(kind) {
     case "container": return ContainerEndpoint()
     default: return GenericEndpoint()
   }
-}
-
-export function endpointKind(url) {
-  if (url.includes("github.com")) {
-    return "github"
-  }
-  if (url.includes(".gc.ca")) {
-    return "web"
-  }
-  if (url.includes("docker")) {
-    return "container"
-  }
-  return "generic"
 }
