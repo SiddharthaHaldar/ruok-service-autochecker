2798b99b7c64a5bcb659112fbbc7f56d53b46a21
Collin Brown <collin.j.brown95@gmail.com>
2023-11-03 18:41:01 +0000

    refactor(graph-updater): change graph assembly + writing logic

--- graph-updater/index.js
+++ graph-updater/index.js
@@ -1,6 +1,10 @@
 import { connect, JSONCodec } from 'nats'
 
-import { getEndpoint } from './src/endpoint.js';
+import { GraphQLClient, gql } from 'graphql-request';
+
+import { getEndpoint, getEndpointKind } from "./src/endpoint.js";
+
+import { publishToNats } from './src/utils.js';
 
 import 'dotenv-safe/config.js'
 
@@ -8,9 +12,12 @@ const {
   NATS_URL,
 } = process.env;
 
-
 const NATS_SUB_STREAM = "EventsUpdate" // Note - for checks that need branch, the substream will be different (right now blanketing with 'main')
-const NATS_PUB_STREAM = "EventScanner" // Note - for checks that need branch - the pubstream will be different 
+
+const CONTAINER_ENDPOINT_QUEUE = "EventsScanner.containerEndpoints"
+const WEB_ENDPOINT_QUEUE = "EventsScanner.webEndpoints"
+const GITHUB_ENDPOINT_QUEUE = "EventsScanner.githubEndpoints"
+
 
 // NATs connection 
 const nc = await connect({ servers: NATS_URL, })
@@ -29,13 +36,65 @@ process.on('SIGINT', () => process.exit(0))
       console.log('\n**************************************************************')
       console.log(`Recieved from ... ${message.subject}:\n ${JSON.stringify(endpointEventPayload)}`)
 
-      const { endpoint, endpointKind } = endpointEventPayload
-      // Based on the kind of endpoint being processed, get the appropriate handler
-      const endpointHandler = getEndpoint(endpointKind)
       // Every endpoint handler knows how to get its own graph metadata
       // from its endpoint.
-      endpointHandler.getGraphMetaData(endpointEventPayload)
-      let tmp=1;
+      const endpointKind = getEndpointKind(endpointEventPayload.endpoint)[0];
+      // Each kind of endpoint knows how to get its own metadata (i.e. what endpoints are
+      // related to this endpoint?); polymorphic method getGraphMetadata knows how to
+      // parse the endpointEventPayload object to extract metadata about related endpoints.
+      const endpointHandler = getEndpoint(endpointKind);
+      const newEndpoints = endpointHandler.getGraphMetaData(endpointEventPayload)
+      // Create string serialized array of endpoints associated with this endpoint
+      const newEndpointsString = `["${Array.from(newEndpoints).join('", "')}"]`;
+
+      // Mutation to add a graph for the new endpoints
+      const mutation = gql`
+      mutation {
+        endpoints(urls: ${newEndpointsString}) 
+      }
+      `;
+      // New GraphQL client - TODO: remove hard-coded URL
+      const graphqlClient = new GraphQLClient("http://localhost:4000/graphql")
+      // Write mutation to GraphQL API
+      const mutationResponse = await graphqlClient.request(mutation);
+
+      // Now that we've written the new graph to the database, we need to query
+      // the same subgraph since there may be existing nodes in the database
+      // that require a new scan.
+      const query = gql`
+      query {
+        endpoints(urls: ${newEndpointsString}) {
+          url
+        }
+      }
+      `;
+
+      const queryResponse = await graphqlClient.request(query);
+
+      const endpointDispatch = {
+        githubEndpoint: [],
+        webEndpoint: [],
+        containerEndpoint: [],
+      }
+
+      // TODO :also graph relation updater needs to know how to figure out what kind of
+      // url each endpoint is (e.g. github endpoint, web endpoint, etc.)
+      for (let i = 0; i < queryResponse.endpoints.length; i++) {
+        const endpointKinds = getEndpointKind(queryResponse.endpoints[i]["url"]);
+        for (let j = 0; j < endpointKinds.length; j++) {
+          endpointDispatch[endpointKinds[j]].push(queryResponse.endpoints[i]["url"]);
+        }
+      }
+
+      // Queue up new endpoints to be analyzed by the appropriate scanners
+      let tmp = 1;
+      await publishToNats(nc, CONTAINER_ENDPOINT_QUEUE, endpointDispatch["containerEndpoint"]);
+      await publishToNats(nc, WEB_ENDPOINT_QUEUE, endpointDispatch["webEndpoint"]);
+      await publishToNats(nc, GITHUB_ENDPOINT_QUEUE, endpointDispatch["githubEndpoint"])
+
+      // TODO: anything under event collectors should not include any extra metadata beyond
+      // the URL itself, because any given event endpoint won't necessarily include info about
+      // what kind of other endpoitns its connected to.
     }
   })();
 
