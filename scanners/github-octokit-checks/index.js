d3892c3e8360c892ddeeada6ff6421074f64b0ff
Collin Brown <collin.j.brown95@gmail.com>
2023-11-07 16:27:43 +0000

    tidy(scanners): remove arangodb client

--- scanners/github-octokit-checks/index.js
+++ scanners/github-octokit-checks/index.js
@@ -4,33 +4,14 @@ import { connect, JSONCodec } from 'nats'
 import { Octokit } from "octokit"
 import { AllChecksStrategy } from './src/all-checks.js';
 
-import { Database, aql } from "arangojs";
-
 import 'dotenv-safe/config.js'
 
 const {
   NATS_URL,
   GITHUB_TOKEN,
-  DB_NAME,
-  DB_URL,
-  DB_USER,
-  DB_PASS,
+  NATS_SUB_STREAM,
 } = process.env;
 
-// Connect to ArangoDB
-const dbConfig = {
-  url: DB_URL,
-  databaseName: DB_NAME,
-  auth: { username: DB_USER, password: DB_PASS },
-  createCollection: true,
-};
-
-const db = new Database(dbConfig);
-
-const OWNER = 'PHACDataHub'
-
-const NATS_SUB_STREAM = "EventsScanner" // Note - for checks that need branch, the substream will be different (right now blanketing with 'main')
-const NATS_PUB_STREAM = "gitHub.saveToDatabase.octokit" // Note - for checks that need branch - the pubstream will be different 
 // Also note - this will be appended with repo name when published. 
 // Authenicate with GitHub 
 const octokit = new Octokit({ auth: GITHUB_TOKEN, });
@@ -51,28 +32,18 @@ process.on('SIGINT', () => process.exit(0))
 
       console.log('\n**************************************************************')
       console.log(`Recieved from ... ${message.subject}:\n ${JSON.stringify(gitHubEventPayload)}`)
-
-      const { sourceCodeRepository } = gitHubEventPayload
-      const repoName = sourceCodeRepository.split('/').pop()
+      // GitHub urls always follow `github.com/orgName/repoName`, so from this
+      // structure we can construct the org name and repo name.
+      const prefix = (new URL(gitHubEventPayload.endpoint)).pathname.split("/")
+      const orgName = prefix[1];
+      const repoName = prefix[2];
 
       const checkName = 'allChecks' // have this passed in in future - default to all-checks
-      // const checkName = 'allLanguages' 
-      // const checkName = 'getRepoDetails' 
       const branchName = 'main' // TODO - come back for this after initial pass - when picked up by repodetails
-      const check = new AllChecksStrategy(repoName, OWNER, octokit, branchName);
+      const check = new AllChecksStrategy(repoName, orgName, octokit, branchName);
 
-      const payload = await check.formatResponse(check)
-      const subject = `${NATS_PUB_STREAM}.${checkName}.${repoName}`
-
-      const upsertQuery = aql`
-        INSERT {
-          _key: ${repoName},
-          githubOctokitChecks: ${payload}
-        }
-        IN dataServices
-        OPTIONS { overwriteMode: "update"}
-      `
-      const results = await db.query(upsertQuery)
+      const payload = await check.formatResponse(check);
+      let tmp = 1;
     }
   })();
 
