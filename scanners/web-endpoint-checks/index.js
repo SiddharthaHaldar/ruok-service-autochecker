a32226daf67e968f9269c2b7b7c38bd11ec8da8a
Collin Brown <collin.j.brown95@gmail.com>
2023-11-08 20:04:51 +0000

    refactor(scanner): write to graphql API

--- scanners/web-endpoint-checks/index.js
+++ scanners/web-endpoint-checks/index.js
@@ -1,24 +1,21 @@
-import { connect, JSONCodec} from 'nats'
-import { Database } from "arangojs";
-import { GraphQLClient } from 'graphql-request'
-import { getPages} from './src/get-url-slugs.js'
+import { connect, JSONCodec } from 'nats'
+import { GraphQLClient, gql } from 'graphql-request'
+import { getPages } from './src/get-url-slugs.js'
 import { isWebEndpointType } from './src/check-endpoint-type.js'
 import { evaluateAccessibility } from './src/accessibility-checks.js'
 import puppeteer from 'puppeteer';
 import 'dotenv-safe/config.js'
 
-const { 
-    NATS_URL,
-    API_URL,
-  } = process.env;
-  
-const NATS_SUB_STREAM="EventsScanner"
+const {
+  NATS_URL,
+  GRAPHQL_URL,
+  NATS_SUB_STREAM,
+} = process.env;
 
-// API connection 
-const graphQLClient = new GraphQLClient(API_URL);
+
 
 // NATs connection 
-const nc = await connect({ servers: NATS_URL,})
+const nc = await connect({ servers: NATS_URL, })
 const jc = JSONCodec()
 
 const sub = nc.subscribe(NATS_SUB_STREAM)
@@ -27,50 +24,78 @@ console.log('🚀 Connected to NATS server - listening on ...', sub.subject, "ch
 process.on('SIGTERM', () => process.exit(0))
 process.on('SIGINT', () => process.exit(0))
 
-;(async () => {
+  ; (async () => {
 
-  const browser = await puppeteer.launch({
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    headless: 'new'
-  });
- 
-  for await (const message of sub) {
-    const webEventPayload  = await jc.decode(message.data)
-    console.log(webEventPayload)
-    const { webEndpoints } = webEventPayload 
-    
-    console.log(webEndpoints)   
+    const browser = await puppeteer.launch({
+      executablePath: '/usr/bin/google-chrome',
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      headless: "new",
+    });
 
-    for (const webEndpoint of webEndpoints) {
+    for await (const message of sub) {
+      const webEventPayload = await jc.decode(message.data)
+      console.log(webEventPayload)
+      const { endpoint } = webEventPayload
+
       const pageInstance = await browser.newPage();
       await pageInstance.setBypassCSP(true);
 
-      if (await isWebEndpointType(webEndpoint, pageInstance)) { //filtering for only web endpoints
-        const pages = await getPages(webEndpoint, pageInstance, browser);
+      if (await isWebEndpointType(endpoint, pageInstance)) { //filtering for only web endpoints
+        const pages = await getPages(endpoint, pageInstance, browser);
         const webEndpointAxeResults = {}  //form response
-    
         for (const pageToEvaluate of pages) {
           console.log('Evaluating page: ', pageToEvaluate)
           const axeReport = await evaluateAccessibility(pageToEvaluate, pageInstance, browser)
-  
-          if (!webEndpointAxeResults[webEndpoint]) {
-            webEndpointAxeResults[webEndpoint] = {};
+
+          for (let i = 0; i < axeReport.length; i++) {
+            const camelize = s => s.replace(/-./g, x => x[1].toUpperCase())
+            const violation = axeReport[i];
+            const violationKey = Object.keys(violation)[0];
+            const violationValue = violation[violationKey];
+            const violationKeyCamelCase = camelize(violationKey);
+            if (!webEndpointAxeResults[pageToEvaluate]) {
+              webEndpointAxeResults[pageToEvaluate] = {}
+            }
+            webEndpointAxeResults[pageToEvaluate][violationKeyCamelCase] = {
+              checkPasses: violationValue.checkPasses,
+              metadata: violationValue.metadata,
+            }
           }
-          webEndpointAxeResults[webEndpoint][pageToEvaluate] = axeReport;
-
         }
-        // SAVE to ArangoDB through API
-        // const upsertService = await upsertClonedGitHubScanIntoDatabase(productName, sourceCodeRepository, results, graphQLClient)
-
-        console.log(JSON.stringify(webEndpointAxeResults, null, 2))
-        // console.log(webEndpointAxeResults)
+        const accessibilityPages = Object.keys(webEndpointAxeResults).map(page => {
+          return {
+            url: page,
+            areaAlt: webEndpointAxeResults[page].areaAlt,
+            ariaBrailleEquivalent: webEndpointAxeResults[page].ariaBrailleEquivalent,
+            ariaCommandName: webEndpointAxeResults[page].ariaCommandName,
+            ariaHiddenFocus: webEndpointAxeResults[page].ariaHiddenFocus,
+            ariaInputField: webEndpointAxeResults[page].ariaInputField,
+            ariaMeterName: webEndpointAxeResults[page].ariaMeterName,
+            // ...webEndpointAxeResults[page],
+          }
+        })
+        const mutation = gql`
+            mutation {
+              webEndpoint(
+                endpoint: {
+                  url: "${endpoint}"
+                  kind: "Web"
+                  accessibility: ${JSON.stringify(accessibilityPages, null, 4).replace(/"([^"]+)":/g, '$1:')}
+                }
+              )
+            }
+            `;
+        // API connection 
+        const graphQLClient = new GraphQLClient(GRAPHQL_URL);
+        // Write mutation to GraphQL API
+        const mutationResponse = await graphQLClient.request(mutation);
+        console.info("wrote mutation to GraphQL API with response", mutationResponse);
         await pageInstance.close()
       }
     }
-  }
-  await browser.close()
+    await browser.close()
 
-})();
+  })();
 
 
 await nc.closed();
