f940c4e185804db22eed46ffeee6533a56506779
Collin Brown <collin.j.brown95@gmail.com>
2023-11-09 21:30:55 +0000

    feat(ui): add proxy dev server - reason is to avoid setting CORS header - just proxy to localhost:4000/graphql instead

--- ui/rspack.config.js
+++ ui/rspack.config.js
@@ -36,5 +36,13 @@ module.exports = {
                 }
             }
         ]
+    },
+    devServer: {
+        proxy: {
+            '/graphql': {
+                target: 'http://localhost:4000',
+                changeOrigin: true,
+            }
+        }
     }
 };
