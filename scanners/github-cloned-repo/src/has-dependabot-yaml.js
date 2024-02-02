5adcc3450d45a73e8d7ebd848f8095d6aca2a239
Lila Kelland <38951930+LilaKelland@users.noreply.github.com>
2024-02-02 14:06:41 -0500

    Review scan for ruok, and strengthen results (#106)
    * removed version for curl in github-webhook-server dockerfile as it didn't work when it was in place - strangely.
    
    * updated dependencies, changed has-dependabot.yaml to check in the .github folder

--- scanners/github-cloned-repo/src/has-dependabot-yaml.js
+++ scanners/github-cloned-repo/src/has-dependabot-yaml.js
@@ -2,13 +2,21 @@ import { CheckOnClonedRepoInterface } from './check-on-cloned-repo-interface.js'
 import { glob } from 'glob'
 import path from 'path';
 
+// export async function hasDependabotYaml(clonedRepoPath) {
+//     // search dependabot.yaml or dependabot.yml anywhere in path
+//     const dependabotFile = glob.sync(path.join(clonedRepoPath, '**', 'dependabot.y*')); 
+//     console.log('******************************', dependabotFile)
+//     // console.log(dependabotFile.length > 0)
+//     return dependabotFile.length > 0;
+// } 
+
 export async function hasDependabotYaml(clonedRepoPath) {
     // search dependabot.yaml or dependabot.yml anywhere in path
-    const dependabotFile = glob.sync(path.join(clonedRepoPath, '**', 'dependabot.y*')); 
-    // console.log('******************************', dependabotFile)
-    // console.log(dependabotFile.length > 0)
+    const dependabotFile = glob.sync(path.join(clonedRepoPath, '**', '.github', '**', 'dependabot.y*'), { nocase: true }); 
+    console.log('Dependabot files found:', dependabotFile.length > 0);
     return dependabotFile.length > 0;
-} 
+}
+
 
 export class HasDependabotYaml extends CheckOnClonedRepoInterface {
     constructor(repoName, clonedRepoPath) { 
